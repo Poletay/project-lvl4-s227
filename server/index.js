0340ee62abc226c0d7983b11290f2f3ed256b897
Aleksey Smirnov <lirien42@gmail.com>
2018-05-13 16:02:28 +0300

    try to add favicon

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ export default () => {
   app.keys = ['some secret hurr'];
   app.use(session(app));
   app.use(bodyParser());
-  app.use(favicon(path.join(__dirname, '..', 'assets/favicon.ico')));
+  app.use(favicon(path.join(__dirname, '..', 'assets/favicon-32.png')));
   app.use(middleware({
     config: webpackConfig,
   }));
